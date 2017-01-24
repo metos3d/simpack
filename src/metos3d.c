79dda4f552d11a32cf745269b643f278014387db
jpicau <jpi@informatik.uni-kiel.de>
2017-01-24 20:23:23 +0100

    added ErrorHandler, PetscAbortErrorHandler

--- src/metos3d.c
+++ src/metos3d.c
@@ -23,6 +23,7 @@ int
 main(int argc, char **args)
 {
     PetscInitialize(&argc, &args, PETSC_NULL, PETSC_NULL);
+    PetscPushErrorHandler(PetscAbortErrorHandler, NULL);
     if (argc >= 2) {
         // data type
         Metos3D metos3d;
