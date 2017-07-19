180d3ac93542205980eb49b6ac3ae8dfd41bacd2
jpicau <jpi@informatik.uni-kiel.de>
2017-07-19 22:03:00 +0200

    changed accuracy of output

--- src/metos3d_bgc.c
+++ src/metos3d_bgc.c
@@ -1053,7 +1053,7 @@ Metos3DBGCTracerMonitor(Metos3D *metos3d, Vec *yin)
         VecPointwiseMult(*ywork, *yvolumes, *ywork);
         VecSum(*ywork, &ysum);
         // print out
-        Metos3DDebug(metos3d, kDebugLevel0, "%04d %s%02d, %-10s%s%-8e\n", metos3d->spinupStep, "Tracer: ", itracer+1, metos3d->tracerName[itracer], ", total: ", ysum);
+        Metos3DDebug(metos3d, kDebugLevel0, "%04d %s%02d, %-10s%s%-24.16e\n", metos3d->spinupStep, "Tracer: ", itracer+1, metos3d->tracerName[itracer], ", total: ", ysum);
     }
     // free work vec
     VecDestroyVecs(1, &ywork);
@@ -1106,7 +1106,7 @@ Metos3DBGCDiagMonitor(Metos3D *metos3d)
         // sum up, only
         VecSum(*ywork, &ysum);
         // print out
-        Metos3DDebug(metos3d, kDebugLevel0, "%04d %s%02d, %-10s%s%-8e\n", metos3d->spinupStep, "Diagnostics: ", idiag+1, metos3d->diagName[idiag], ", total: ", ysum);
+        Metos3DDebug(metos3d, kDebugLevel0, "%04d %s%02d, %-10s%s%-24.16e\n", metos3d->spinupStep, "Diagnostics: ", idiag+1, metos3d->diagName[idiag], ", total: ", ysum);
     }
     // free work vec
     VecDestroyVecs(1, &ywork);
